957fd44e561e1034c1cf0543c58c65108582ac32
Jon Abulencia <jon.abulencia@upskill.io>
2019-01-05 14:24:42 -0500

    Finished the working guessing game!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,35 @@
+use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("### Guess the Number! ###");
-    println!("### Enter your number guess below: ###");
+    let secret_num = rand::thread_rng().gen_range(1, 10);
 
-    // mut means mutable
-    let mut guess = String::new();
+    loop {
+        // mut means mutable
+        let mut guess = String::new();
+        println!("### Enter your number guess between 1 and 10 below: ###");
+        io::stdin().read_line(&mut guess) // & means reference here, like a pointer in C
+            .expect("Unable to read your entry...");
+        println!("### You guessed: {}", guess);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Unable to read your entry...");
+        // This is shadowing- multiple representations of the same value
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("That wasn't a number, try again");
+                continue
+            }
+        };
 
-    println!("### You guessed: {}", guess);
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("You got it!!");
+                break;
+            }
+        }
+    }
 }
